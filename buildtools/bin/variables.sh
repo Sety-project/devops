ecde34901d0244dfeca83267889af91d1f02cbeb
vic <victor.houlet@gmail.com>
2022-05-09 16:36:18 +0100

    adding git functions and separating bashrc variables

---
+++ buildtools/bin/variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Stores all local variables for all scripts
+
+export USERNAME=$USER
+export GIT_REPO=git@github.com:Sety-project
+export NAMESPACE=Sety-project
+export PYTHON_REGISTRY="878533356457.dkr.ecr.eu-west-2.amazonaws.com"     # --> Sety AWS
