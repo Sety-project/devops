ae6ff9bfd4a791824c2b40466f1d732f3705c279
vic <victor.houlet@gmail.com>
2022-05-09 18:24:03 +0100

    wrapping up cache_static function

--- buildtools/bin/simex_functions.sh
+++ buildtools/bin/simex_functions.sh
@@ -2,6 +2,7 @@
 
 cache_static() {
 	# Function to cache static_main.json from the ec2
+	# Will pull static_main.json or static_date if date is passed as argument
 	
 	if [[ $# -eq 1 ]] ; then
 		DATE=$1                                                                              
@@ -11,17 +12,17 @@ cache_static() {
 
 	CACHE_LOCATION=/home/$USERNAME/.cache/staticdata
 	REMOTE_LOCATION=/home/$USERNAME/Static/StaticData
-	STATIC_FILE=/home/$USERNAME/Static/StaticData/static_$DATE.json                                                        
+	STATIC_PATH=/home/ec2-user/Static/StaticData                                                        
+	STATIC_FILE=static_$DATE.json
 
 	mkdir -p $CACHE_LOCATION                                                             
 		                                                                         
 	if test -f "$CACHE_LOCATION/$STATIC_FILE"; then                                       
-		echo "$STATIC_FILE found."                                                  
-		return                                                                           
-	else                                                              
-		COMMAND_OUTPUT=`scp -i ~/Downloads/ec2-one.pem ec2-user@ec2-3-8-151-236.eu-west-2.compute.amazonaws.com:$STATIC_FILE $CACHE_LOCATION/`
-	fi                                                                                   
-		                                                                         
+		echo -e "$STATIC_FILE found\nReplacing old file..."                                                                     
+	fi                                 
+	
+	COMMAND_OUTPUT=`scp -i ~/.cache/setykeys/ec2-one.pem ec2-user@ec2-3-8-151-236.eu-west-2.compute.amazonaws.com:$STATIC_PATH/$STATIC_FILE $CACHE_LOCATION/`
+	                                                                                   	                                                                         
 	if [[ $? -eq 0 ]] ; then                                                             
 		echo "Successfully downloaded $STATIC_FILE."                                 
 	fi                                                                                   
