87c21085a39ed2d74321b86c1fac1f7d8d69027f
vic <victor.houlet@gmail.com>
2022-05-11 20:08:21 +0100

    adding j() and jpl() bash functions to jump between repos

--- buildtools/bin/bash_functions.sh
+++ buildtools/bin/bash_functions.sh
@@ -5,3 +5,11 @@ source ~/Sety-project/devops/buildtools/bin/git_functions.sh
 source ~/Sety-project/devops/buildtools/bin/simex_functions.sh
 source ~/Sety-project/devops/buildtools/bin/python_functions.sh
 
+j() {
+    cd ~/Sety-project/$1
+}
+
+jpl() {
+    cd ~/Sety-project/pylibs/$1
+}
+
