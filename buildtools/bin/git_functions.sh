728bb7e4406048276785dd9a318f22e99a1174b1
vic <victor.houlet@gmail.com>
2022-05-11 20:10:55 +0100

    adding function get_git_project_id and get_git_user_id

--- buildtools/bin/git_functions.sh
+++ buildtools/bin/git_functions.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+source ~/Sety-project/devops/buildtools/bin/variables.sh
+
+declare -A LINUX_TO_GIT_USER
+LINUX_TO_GIT_USER=(["vic"]="mol86" ["david"]="daviidarr")
+
 clone_repository() {
 	# Creates and clones an existing git repo
 	
@@ -47,3 +52,62 @@ clone_repository() {
 	echo -e "\nSuccesfully cloned repository $1"
 }
 
+get_git_project_id() {
+   if [[ $# -eq 1 ]] ; then
+        OWNER=$NAMESPACE
+        PROJECT=$1
+   else
+       PROJECT=$1
+       OWNER=$2
+   fi
+
+   curl -u "${LINUX_TO_GIT_USER[$USERNAME]}":$GIT_PAT https://api.github.com/repos/$OWNER/$PROJECT | jq -r '.id'
+}
+
+get_git_user_id() {
+    curl -u "${LINUX_TO_GIT_USER[$1]}":$GIT_PAT https://api.github.com/users/"${LINUX_TO_GIT_USER[$1]}" | jq -r '.id'
+}
+
+
+#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/mol86/pylibs
+#curl -s -X GET --header "PRIVATE-TOKEN: $GITLAB_TOKEN" https://api.github.com/repos/mol86/pylibs
+
+#curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Sety-project/pylibs/issues -d '{"title":"GitHub REST API","body":"Testing GitHub API","assignees":["mol86"],"milestone":1,"labels":["test"]}'
+#curl -X POST -H "Authorization: Bearer $GITHUB_APP_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Sety-project/pylibs/issues -d '{"title":"GitHub REST API","body":"Testing GitHub API","assignees":["mol86"],"milestone":1,"labels":["test"]}'
+
+
+#curl \
+#  -X POST \
+#  -H "Accept: application/vnd.github.v3+json" \
+#  https://api.github.com/repos/OWNER/REPO/issues \
+#  -d '{"title":"Found a bug","body":"I'm having a problem with this.","assignees":["octocat"],"milestone":1,"labels":["bug"]}'
+
+
+
+
+
+#GITHUB_APP_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiAiMTk5MzU5IiwgInN1YiI6ICJtb2w4NiIsICJpYXQiOiAxNjUyMjA1MzI2LCAiZXhwIjogMTY1MjIwNTkwNn0.ndjyaTLBAbl3fh8ufHynXp2BgJHmhs_OvDZMD141nrzP81EQQK6k2mnt2Oz9c6qljdKMfwtA0LAXbHMpAFQAKTQkMQLQgdwo4B7GKV92tAQ9F246UKymOPl7RNA0Ai52-6OEDxMCSZAcOKZpd20aYzXLEuEqHntLuax81VonLxqBUN-EloHVE3cB4kl6zSb2uQmfvyzqhqJBQDr1UlpWnMx7AKKNK4IG4hHQJOd9jlaMoVzyJqIVm_kltcadCuqFefJ86R8FKQD4TrYDjr0N15yxscScu4BI8tnRccOo7uy_SNZV-A3QBpOIOr3yLeg0NcKh0qiqkvf05z1xHuq3dw
+
+#curl -i -X GET -H "Authorization: Bearer $GITHUB_APP_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations
+
+#curl -i -X GET -H "Authorization: Bearer $GITHUB_APP_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/orgs/Sety-project/pylibs
+
+
+
+#curl -i -H "Authorization: Bearer $GITHUB_APP_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app                             # WORKS WHEN JWT TOKEN IS VALID
+#curl -i -H "Authorization: Bearer $GITHUB_APP_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Sety-project/pylibs
+#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Sety-project/pylibs
+
+#GET ALL REPOSITORIES --> WORKS
+#curl -u mol86:$GIT_PAT https://api.github.com/user
+#curl -u mol86:$GIT_PAT https://api.github.com/repos/mol86/pylibs => does not work
+#curl -u mol86:$GIT_PAT https://api.github.com/orgs/Sety-project/repos => WORKS
+
+# GET A REPOSITORY --> WORKS
+#curl -u mol86:$GIT_PAT https://api.github.com/repos/Sety-project/pylibs
+
+
+
+
+#Message : Validation fails
+#curl -u mol86:$GIT_PAT -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Sety-project/pylibs/issues -d '{"title":"GitHub REST API","body":"Testing GitHub API","assignees":["mol86"],"milestone":1,"labels":["test"]}'
