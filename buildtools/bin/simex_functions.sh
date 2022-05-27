801073eec00f2214e29d3f15df158fb9c346d267
vic <victor.houlet@gmail.com>
2022-05-27 09:53:23 +0000

    cache_mktdata should delete previous repo before downloading since it erases anyway

--- buildtools/bin/simex_functions.sh
+++ buildtools/bin/simex_functions.sh
@@ -47,6 +47,7 @@ cache_mktdata(){
 	CACHE_LOCATION=/home/$USERNAME/mktdata
 	MKTDATA_PATH=/home/ec2-user/mktdata
 	
+	rm -rf $CACHE_LOCATION
 	mkdir -p $CACHE_LOCATION
 
 	COMMAND_OUTPUT=`scp -rv -i ~/.cache/setykeys/ec2-one.pem ec2-user@$ELASTIC_IPV4DNS:$MKTDATA_PATH/* $CACHE_LOCATION/`
