c4fcda6d854434329e16f4b2eb0ec57cd8f99fbc
vic <victor.houlet@gmail.com>
2022-06-02 15:45:30 +0000

    removing container after execution, adding a container name

--- buildtools/bin/python_functions.sh
+++ buildtools/bin/python_functions.sh
@@ -51,15 +51,15 @@ pyrun_static(){
 }
 
 pyrun_histfeed(){
-	pyrun histfeed -e EXCHANGE_NAME="ftx" -e RUN_TYPE="build" -e UNIVERSE="all" -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v ~/mktdata:/home/ec2-user/mktdata -v /tmp:/tmp
+	pyrun histfeed --rm --name=histfeed_worker -e EXCHANGE_NAME="ftx" -e RUN_TYPE="build" -e UNIVERSE="all" -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v ~/mktdata:/home/ec2-user/mktdata -v /tmp:/tmp
 }
 
 pyrun_pfoptimizer(){
-	pyrun pfoptimizer -e USERNAME=$USERNAME -e EXCHANGE_NAME="ftx" -e RUN_TYPE="sysperp" -v ~/mktdata:/home/ec2-user/mktdata -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v /tmp:/tmp $PYTHON_REGISTRY/pfoptimizer:latest
+	pyrun pfoptimizer --rm --name=pfoptimizer_worker -e EXCHANGE_NAME="ftx" -e RUN_TYPE="sysperp" -v ~/mktdata:/home/ec2-user/mktdata -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v /tmp:/tmp
 }
 
 pyrun_tradeexecutor(){
-	pyrun tradeexecutor -e USERNAME=$USERNAME -e EXCHANGE_NAME="ftx" -e RUN_TYPE="sysperp" SUBACCOUNT='SysPerp' -v ~/mktdata:/home/ec2-user/mktdata -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v /tmp:/tmp $PYTHON_REGISTRY/tradeexecutor:latest
+	pyrun tradeexecutor --rm --name=tradeexecutor_worker -e EXCHANGE_NAME="ftx" -e RUN_TYPE="sysperp" SUBACCOUNT='SysPerp' -v ~/mktdata:/home/ec2-user/mktdata -v ~/.cache/setyvault:/home/ec2-user/.cache/setyvault -v ~/config/prod:/home/ec2-user/config -v /tmp:/tmp
 }
 
 # [Shortcut helpers to debug]
