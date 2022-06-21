5cd1f1204b35a185779c52d199ddef872ce651f2
vic <victor.houlet@gmail.com>
2022-06-21 14:25:33 +0000

    adding script for local machine boot instructions

---
+++ buildtools/init/boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/Sety-project/devops/buildtools/bin/simex_functions.sh
+source ~/Sety-project/devops/buildtools/bin/git_functions.sh
+source ~/Sety-project/devops/buildtools/bin/python_functions.sh
+source ~/Sety-project/devops/buildtools/bin/bash_functions.sh
+
+cache_tmp
+gpa
