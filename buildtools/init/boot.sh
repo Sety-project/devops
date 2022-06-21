528701fdcbfea56dbb10d5bb2a2dca477aa04a94
vic <victor.houlet@gmail.com>
2022-06-21 15:22:06 +0000

    log boot up results

--- buildtools/init/boot.sh
+++ buildtools/init/boot.sh
@@ -5,5 +5,5 @@ source ~/Sety-project/devops/buildtools/bin/git_functions.sh
 source ~/Sety-project/devops/buildtools/bin/python_functions.sh
 source ~/Sety-project/devops/buildtools/bin/bash_functions.sh
 
-cache_tmp
-gpa
+cache_tmp >> /tmp/boot.log
+gpa >> /tmp/boot.log
