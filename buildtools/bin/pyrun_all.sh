6e1eee519512de6946246a687a203c21e67bc074
david <davidrelkin@gmail.com>
2022-07-14 16:15:34 +0000

    -it

--- buildtools/bin/pyrun_all.sh
+++ buildtools/bin/pyrun_all.sh
@@ -6,7 +6,7 @@ source ~/.bashrc
 pyrun_histfeed ftx
 
 histfeed_status="$(docker container wait histfeed_worker_ftx)"
-echo "Status code of histfeed_worker: $histfeed_status"
+echo "Status code of histfeed_worker: "$histfeed_status""
 
 # Run pfoptimizer only if histfeed returns 0
 if [[ histfeed_status -eq 0 ]]; then
@@ -17,9 +17,3 @@ fi;
 # Run pnlexplain every hour. Removed -e USERNAME=$USERNAME
 pyrun_riskpnl ftx SysPerp
 pyrun_riskpnl ftx debug
-
-riskpnl_status="$(docker container wait riskpnl_worker)"
-echo "Status code of riskpnl_worker: riskpnl_status"
-
-# Run tradeexecutor only if pfoptimizer returns 0
-# Run tradeexecutor if files date less than 1 hour ago... we can design what we want...
