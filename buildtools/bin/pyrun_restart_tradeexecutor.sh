f457f518569dea5f7508c19332fbd3e8c7b98482
david <davidrelkin@gmail.com>
2022-07-23 21:36:35 +0000

    don't stop any executor. redundant ones won't start anyway

--- buildtools/bin/pyrun_restart_tradeexecutor.sh
+++ buildtools/bin/pyrun_restart_tradeexecutor.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 
 # Could use docker-compose at some point...
 # Run histfeed
-echo "stop tradeexecutor***"
-pystop tradeexecutor
+#echo "stop tradeexecutor***"
+#pystop tradeexecutor
 echo "restart tradeexecutor on current shards"
 pyrun_tradeexecutor $1 $2
