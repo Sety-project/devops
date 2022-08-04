e74c7e6a5b10129a3de6793dfc249e4e86174b1a
david <davidrelkin@gmail.com>
2022-08-04 00:40:01 +0000

    save

--- buildtools/bin/pyrun_restart_tradeexecutor.sh
+++ buildtools/bin/pyrun_restart_tradeexecutor.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 source ~/.bashrc
 
-# Could use docker-compose at some point...
-# Run histfeed
 #echo "stop tradeexecutor***"
 #pystop tradeexecutor
-echo "restart tradeexecutor on current shards"
+echo "start tradeexecutor on current shards"
 pyrun_tradeexecutor $1 $2
