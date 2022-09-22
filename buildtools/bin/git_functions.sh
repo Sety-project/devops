bdbde2f08be90fd6ca4e8d8d1835375d348e26dc
daviidarr <davidrelkin@gmail.com>
2022-09-22 14:50:29 +0000

    Revert "dummy"
    This reverts commit d23e85b828ee5ebdc500f127382680a38a724edc.

--- buildtools/bin/git_functions.sh
+++ buildtools/bin/git_functions.sh
@@ -5,7 +5,6 @@ source ~/Sety-project/devops/buildtools/bin/variables.sh
 declare -A LINUX_TO_GIT_USER
 LINUX_TO_GIT_USER=(["vic"]="mol86" ["david"]="daviidarr")
 
-
 clone_repository() {
 	# Creates and clones an existing git repo
 	
