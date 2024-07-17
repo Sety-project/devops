772ec167e46c05dc1871eb8d4236e164289485fc
daviidarr <nononymous@gmail.com>
2024-07-17 21:18:28 +0200

    script attempts..

--- buildtools/bin/pyrun_functions.sh
+++ buildtools/bin/pyrun_functions.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 export ECR_REGION=eu-west-2
 
@@ -64,6 +64,29 @@ pyrun() {
 	  -v /tmp:/tmp \
 	  --network host $PYTHON_REGISTRY/$PYTHON_PROJECT:latest
 	fi
+}
+
+pyrun_actualyield() {
+    # because the run.sh demands them, all params must be passed incl optionals. "not_passed" will apply default from python script
+    # gpa
+
+    docker_login
+    IS_DOCKER_RUNNING=`systemctl status docker | grep Active | grep running | wc -l`
+    if [[ $IS_DOCKER_RUNNING -eq 0 ]] ; then
+    sudo /bin/systemctl start docker.service
+    fi
+
+    docker rm $(docker ps --filter status=exited -q)
+    docker pull $PYTHON_REGISTRY/actualyield:latest
+
+    # they're in fact both running without detach falg...
+    docker run -e USERNAME=$USERNAME "${@}" \
+    -v ~/actualyield:/home/ubuntu/actualyield \
+    -v ~/actualyield/data:/home/ubuntu/actualyield/data \
+    -v ~/.cache/setyvault:/home/ubuntu/.cache/setyvault \
+    -v ~/actualyield/.streamlit:/home/ubuntu/actualyield/.streamlit \
+    -v /tmp:/tmp \
+    --network host $PYTHON_REGISTRY/actualyield:latest
 }
 
 #################################
